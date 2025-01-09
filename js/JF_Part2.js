3b459b5854005274915453ba3ba4a7343b29a5fb
LeTThuyHien <thuyhienle1664@gmail.com>
2025-01-09 18:49:51 +0700

    JavaScript Fundamentals – Part 2 coding 3

--- js/JF_Part2.js
+++ js/JF_Part2.js
@@ -44,3 +44,39 @@ console.log('Bills:', bills);
 console.log('Tips:', tips);
 console.log('Total:', total);
 
+console.log("\n");
+console.log("Coding Challenge #3");
+let Mark = {
+    fullname: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.BMI = (this.mass / this.height ** 2).toFixed(1);
+    }
+
+}
+let John = {
+    fullname: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.BMI = (this.mass / this.height ** 2).toFixed(1);
+    }
+}
+// function calcBMI(person){
+//     return person.mass / person.height ** 2;
+// }
+Mark.calcBMI();
+John.calcBMI();
+if (Mark.BMI > John.BMI) {
+    console.log(`BMI của ${Mark.fullname} (${Mark.BMI}) cao hơn BMI của ${John.fullname} (${John.BMI})`);
+}
+else if (John.BMI > Mark.BMI) {
+    console.log(`BMI của ${John.fullname} (${John.BMI}) cao hơn BMI của ${Mark.fullname} (${Mark.BMI})`);
+}
+else {
+    console.log(`BMI của ${Mark.fullname} (${Mark.BMI}) và ${John.fullname} (${John.BMI}) bằng nhau`);
+}
+
+
+
