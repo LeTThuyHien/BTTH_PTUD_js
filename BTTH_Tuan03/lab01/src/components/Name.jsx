1e7cdc3a9cb6c73c1d62947229c2a3749ad5fddd
pharmacy <thuyhienle1664@gmail.com>
2025-02-20 20:01:00 +0700

    Add BTTH_PTGDDUD_Lab01_testName

--- BTTH_Tuan03/lab01/src/components/Name.jsx
+++ BTTH_Tuan03/lab01/src/components/Name.jsx
@@ -1,41 +1,42 @@
 import { useState } from 'react';
+import './Name.css';
 
-function Name() {
-  const [name, setName] = useState('');
-  const [disabled, setDisabled] = useState('');
-  const [result, setResult] = useState('');
+    function Name() {
+        const [name, setName] = useState('');
+        const [disabled, setDisabled] = useState('');
+        const [result, setResult] = useState('');
 
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
+        function handleNameChange(e) {
+            setName(e.target.value);
+        }
 
-  function handleClick() {
-    setDisabled(name);
-    setResult(name);
-  }
+        function handleClick() {
+            setDisabled(name);
+            setResult(name);
+        }
 
-  function handleReset() {
-    setDisabled('');
-    setName('');
-    setResult('');
-  }
+        function handleReset() {
+            setDisabled('');
+            setName('');
+            setResult('');
+        }
 
-  return (
-    <div className="NameComponent">
-      <input
-        type="text"
-        onChange={handleNameChange}
-        value={name}
-        placeholder="Nhập tên..."
-        disabled={disabled !== ''}
-      />
-      <div>
-        <button onClick={handleClick}>Click</button>
-        <button onClick={handleReset}>Reset</button>
-      </div>
-      <span>{result}</span>
-    </div>
-  );
-}
+        return (
+            <div className="NameComponent">
+                <input
+                    type="text"
+                    onChange={handleNameChange}
+                    value={name}
+                    placeholder="Nhập tên..."
+                    disabled={disabled !== ''}
+                />
+                <div>
+                    <button onClick={handleClick}>Click</button>
+                    <button onClick={handleReset}>Reset</button>
+                </div>
+                <span>{result}</span>
+            </div>
+        );
+    }
 
 export default Name;
